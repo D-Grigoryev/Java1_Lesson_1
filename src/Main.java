b2166513b16da676ab5bfc7b935111f3f3c98f17
D-Grigoryev <dinvest4@gmail.com>
2021-03-07 20:30:40 +0300

    Добавлено задание к 8-1 уроку: Написать проект игры крестики нолики на java SWING (3 на 3 размер поля)

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 
-        Kitty cat = new Kitty("Barsik", 6);
-        Plate plate = new Plate(100);
-        plate.info();
-        cat.eat(plate);
-        plate.info();
-
-        Kitty [] cats = new Kitty[5];
-        cats[0]=new Kitty("BOSS", 10);
-        cats[1]=new Kitty("BARSIK", 8);
-        cats[2]=new Kitty("KOMP0T", 11);
-        cats[3]=new Kitty("TOM", 17);
-        cats[4]=new Kitty("RIKOLIT", 70);
-
-        for (Kitty c1: cats) {
-            c1.eat(plate);
-        }
-        plate.info();
-        plate.upEat(plate.food);
-        plate.info();
-
+        MainWindow windiow = new MainWindow();
     }
 
 
